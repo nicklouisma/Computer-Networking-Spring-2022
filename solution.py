59e28e6c60823330d8228a174613cf94ac5e2d96
Nick Louisma <60583235+nicklouisma@users.noreply.github.com>
2022-02-26 13:37:08 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -51,7 +51,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     data = "DATA\r\n"
     clientSocket.send(data.encode())
     recvDATA = clientSocket.recv(1024).decode()
-    if recvDATA[:3] != '550':
+    if recvDATA[:3] != '354':
         clientSocket.close()
     # Fill in end
 
