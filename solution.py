19a2dcba24eccd65b790dbb43e6a7cbbe66bac11
Nick Louisma <60583235+nicklouisma@users.noreply.github.com>
2022-04-16 20:14:05 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,6 @@ import struct
 import time
 import select
 import binascii
-# Should use stdev
 from statistics import *
 ICMP_ECHO_REQUEST = 8
 
@@ -53,7 +52,6 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         recPacket, addr = mySocket.recvfrom(1024)
 
         # Fill in start
-        # Fetch the ICMP header from the IP packet
         icmpHeader = recPacket[20:28]
         icmpType, code, myChecksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
         if icmpType != 8 and packetID == ID:
@@ -70,20 +68,14 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
 
 
 def sendOnePing(mySocket, destAddr, ID):
-    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
 
     myChecksum = 0
-    # Make a dummy header with a 0 checksum
-    # struct -- Interpret strings as packed binary data
+
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     data = struct.pack("d", time.time())
-    # Calculate the checksum on the data and the dummy header.
     myChecksum = checksum(header + data)
 
-    # Get the right checksum, and put in the header
-
     if sys.platform == 'darwin':
-        # Convert 16-bit integers from host to network  byte order
         myChecksum = htons(myChecksum) & 0xffff
     else:
         myChecksum = htons(myChecksum)
@@ -92,17 +84,11 @@ def sendOnePing(mySocket, destAddr, ID):
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     packet = header + data
 
-    mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
-
-
-    # Both LISTS and TUPLES consist of a number of objects
-    # which can be referenced by their position number within the object.
+    mySocket.sendto(packet, (destAddr, 1))
 
 def doOnePing(destAddr, timeout):
     icmp = getprotobyname("icmp")
 
-
-    # SOCK_RAW is a powerful socket type. For more details:   https://sock-raw.org/papers/sock_raw
     mySocket = socket(AF_INET, SOCK_RAW, icmp)
 
     myID = os.getpid() & 0xFFFF  # Return the current process i
@@ -113,23 +99,18 @@ def doOnePing(destAddr, timeout):
 
 
 def ping(host, timeout=1):
-    # timeout=1 means: If one second goes by without a reply from the server,  	
-    # the client assumes that either the client's ping or the server's pong is lost
+
     dest = gethostbyname(host)
     print("Pinging " + dest + " using Python:")
     print("")
     delay_float = array.ArrayType('f')
-    #Send ping requests to a server separated by approximately one second
-    #Add something here to collect the delays of each ping in a list so you can calculate vars after your ping
     
-    for i in range(0,4): #Four pings will be sent (loop runs for i=0, 1, 2, 3)
+    for i in range(0,4): 
         delay = doOnePing(dest, timeout)
         delay_float.append(delay)
         print(delay)
-        time.sleep(1)  # one second
-        
-    #You should have the values of delay for each ping here; fill in calculation for packet_min, packet_avg, packet_max, and stdev
-    #vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
+        time.sleep(1)
+
     packet_min = min(delay_float)
     packet_max = max(delay_float)
     packet_avg = (sum(delay_float))/(len(delay_float))
